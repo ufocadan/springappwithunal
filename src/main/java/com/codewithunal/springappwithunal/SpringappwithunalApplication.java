384e69ee1cf3da7899bc245f0e4326f15f73a77e
unalfocadan <unalfocadan@gmail.com>
2024-06-09 17:12:27 +0300

    Initial commit with project structure and setup

---
+++ src/main/java/com/codewithunal/springappwithunal/SpringappwithunalApplication.java
@@ -0,0 +1,13 @@
+package com.codewithunal.springappwithunal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringappwithunalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringappwithunalApplication.class, args);
+	}
+
+}
